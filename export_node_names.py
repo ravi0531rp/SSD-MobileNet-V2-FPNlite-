976b8927f39e7fdef17ad1534b038e39b152feee
Ravi Prakash <ravi.0531.rp@gmail.com>
2021-10-02 12:07:04 +0530

    nodeNamesExporterAdded

---
+++ export_node_names.py
@@ -0,0 +1,19 @@
+## print out all the node names ; edit the GRAPH_PB_PATH as per your exported grap
+
+import tensorflow as tf
+
+GRAPH_PB_PATH = '/content/drive/MyDrive/SSD_EXPERIMENT5/output_inf_graph_SSD_FPN_COLAB_v2/frozen_inference_graph.pb'
+with tf.Session() as sess:
+   print("load graph")
+   with tf.io.gfile.GFile(GRAPH_PB_PATH,'rb') as f:
+       graph_def = tf.compat.v1.GraphDef()
+   graph_def.ParseFromString(f.read())
+   sess.graph.as_default()
+   tf.import_graph_def(graph_def, name='')
+   graph_nodes=[n for n in graph_def.node]
+   names = []
+   with open("/content/drive/MyDrive/SSD_EXPERIMENT5/output_inf_graph_SSD_FPN_COLAB_v2/transfer.txt",'a') as fileWriter:
+    for t in graph_nodes:
+        names.append(t.name)
+        print(t.name)
+        fileWriter.write(t.name+"\n")
